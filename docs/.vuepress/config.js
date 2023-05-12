41470b9521412cda18867df0f9570bf66b1d3809
meijifu <7531336@qq.com>
2023-05-12 18:42:26 +0800

    修改笔记文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  themeConfig: {
+    logo: '/images/logo.svg',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
