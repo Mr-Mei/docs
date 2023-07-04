445f0583b8dd2a43826f851d51aef286b2d6a0b6
meijifu <7531336@qq.com>
2023-07-04 17:37:32 +0800

    修改配置文件

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/", // docs---github仓库名
+import {defineConfig} from "vite"
+
+export default defineConfig({
+  base: "./"  // 默认路径是/   配置文件的根目录为相对路径
 })
